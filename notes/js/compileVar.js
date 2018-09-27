d1bed75767526b5e6077967625b2e1b7989ffd85
jianhuinie <niejianhui@baijiahulian.com>
2018-09-27 16:02:27 +0800

    ppts

--- notes/js/compileVar.js
+++ notes/js/compileVar.js
@@ -12,17 +12,30 @@ function test() {
  }
  test();
 
-// console.log(a);
+console.log(a);
 // a();
-// var a = 3;
-// function a(){
-//     console.log(10);
-// }   
-// console.log(a);
+var a = 3;
+function a() {
+    console.log(10);
+}   
+console.log(a);
 // a=6;
 // a();
 
-console.log(a); 
-var a = 3;
-function a(){};
-console.log(a);  //3
+// console.log(a); 
+// var a = 3;
+// function a(){};
+// console.log(a);  //3
+
+
+
+var name = 'larry';
+function testA() {
+    console.log(name);
+    var name = 'niejianhui';
+    testB();
+}
+function testB() {
+    console.log(name);
+}
+testA();
